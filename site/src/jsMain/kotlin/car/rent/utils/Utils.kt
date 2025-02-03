0c86f8ec6e7bd691bb1533d848803beb5f825ee9
Amal Nuritdinkhodzhaev <85354530+JustAmalll@users.noreply.github.com>
2025-02-03 23:40:52 +0600

    Update

--- site/src/jsMain/kotlin/car/rent/utils/Utils.kt
+++ site/src/jsMain/kotlin/car/rent/utils/Utils.kt
@@ -7,6 +7,7 @@ import com.varabyte.kobweb.compose.ui.modifiers.onClick
 fun Modifier.onClicked(action: (SyntheticMouseEvent) -> Unit): Modifier = then(
     Modifier.onClick { event ->
         event.stopPropagation()
+        event.preventDefault()
         action(event)
     }
 )
